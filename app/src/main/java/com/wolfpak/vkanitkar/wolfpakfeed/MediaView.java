bc4aeba0a1d3fae6ed12fae7098e7ffd4b5172a3
aadesh <aadeshp95@gmail.com>
2015-07-21 20:38:53 -0400

    added custom MediaView class that extends RelativeLayout

---
+++ app/src/main/java/com/wolfpak/vkanitkar/wolfpakfeed/MediaView.java
@@ -0,0 +1,95 @@
+package com.wolfpak.vkanitkar.wolfpakfeed;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
+
+import com.squareup.picasso.Picasso;
+
+public class MediaView extends RelativeLayout {
+    public enum LikeStatus {
+        Like,
+        Neutral,
+        Dislike
+    };
+
+    // Private variables
+    private ImageView mediaImageView;
+    private VideoView mediaVideoView;
+
+    private View likeStatusOverlayView;
+
+    // Constructors
+    public MediaView(Context context) {
+        super(context);
+        baseInit();
+    }
+
+    public MediaView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        baseInit();
+    }
+
+    public MediaView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        baseInit();
+    }
+
+    /**
+     * Handles Changing Opaque View on Top of ImageView/VideoView Based on LikeStatus
+     * - Like: Green
+     * - Dislike: Red
+     * - Neutral: Transparent
+     *
+     * @param likeStatus Status of the view
+     */
+    public void setLikeStatus(LikeStatus likeStatus) {
+        switch (likeStatus) {
+            case Like:
+                this.likeStatusOverlayView.setBackgroundColor(Color.GREEN);
+                break;
+
+            case Dislike:
+                this.likeStatusOverlayView.setBackgroundColor(Color.RED);
+                break;
+
+            default:
+                this.likeStatusOverlayView.setBackgroundColor(Color.TRANSPARENT);
+                break;
+        }
+    }
+
+    /**
+     * Initialize media view based on url and media type
+     *
+     * @param mediaUrl AWS S3 URL for the media
+     * @param isImage boolean of whether or not the mediaUrl is an image
+     */
+    public void setMediaView(Uri mediaUrl, boolean isImage) {
+        if (isImage) {
+            this.mediaImageView.setVisibility(View.VISIBLE);
+            Picasso.with(this.mediaImageView.getContext()).load(mediaUrl).into(this.mediaImageView);
+        } else {
+            this.mediaImageView.setVisibility(View.VISIBLE);
+            this.mediaVideoView.setVideoURI(mediaUrl);
+        }
+    }
+
+    /**
+     * Base Initialization for this class
+     */
+    private void baseInit() {
+        LayoutInflater.from(this.getContext()).inflate(R.layout.media_view, this);
+
+        this.mediaImageView = (ImageView)findViewById(R.id.mediaImageView);
+        this.mediaVideoView = (VideoView)findViewById(R.id.mediaVideoView);
+
+        this.likeStatusOverlayView = (View)findViewById(R.id.likeStatusOverlayView);
+    }
+}
